354bd7dfa08402c29a019df155cf39d6805d9d4a
Anshu Nishad <94797176+TechWhiz0@users.noreply.github.com>
2025-02-22 21:51:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="flex font-medium text-gray-700 gap-2 font-satoshi text-sm">
       <p>
-        2023 <span className="orange_gradient">SummarizeMasters </span>All Rights Reserved
+        2025 <span className="orange_gradient">SummarizeMasters </span>All Rights Reserved
       </p>
       <p>
-        Made by <span className="blue_gradient"> Anshu & Manvi </span>
+        Made by <span className="blue_gradient"> Anshu </span>
       </p>
     </div>
   );
